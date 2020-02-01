5ed4de2b89d862d662ba20689eddacc06a989d53
Victor Marques <victor.atmorning@gmail.com>
2020-02-01 01:34:11 -0300

    Add ex_doc library on development environment

--- mix.exs
+++ mix.exs
@@ -7,7 +7,8 @@ defmodule ExBanking.MixProject do
       version: "0.1.0",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      docs: docs()
     ]
   end
 
@@ -19,6 +20,16 @@ defmodule ExBanking.MixProject do
   end
 
   defp deps do
-    [{:dialyxir, "~> 1.0.0-rc.7", only: [:dev], runtime: false}]
+    [
+      {:dialyxir, "~> 1.0.0-rc.7", only: [:dev], runtime: false},
+      {:ex_doc, "~> 0.21", only: :dev, runtime: false}
+    ]
+  end
+
+  defp docs do
+    [
+      main: "ExBanking",
+      extras: ["README.md"]
+    ]
   end
 end
